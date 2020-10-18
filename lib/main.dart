1cd9d0510464722d5b1e4914a52cb9296b1848d4
Mucyowicyogere <arnaudmucyo3@gmail.com>
2020-10-18 19:13:44 +0200

    me

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,24 @@ class _MyHomeState extends State<MyHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          elevation: 0.0,
-          title: Text(
-            "Kora Rimwe",
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-          ),
+        elevation: 0.0,
+        title: Text(
+          "Kora Rimwe",
+          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
         actions: [
-          IconButton(icon: Icon(Icons.more_vert,color: Colors.white,), onPressed: null),
-          IconButton(icon: Icon(Icons.share,color: Colors.white,), onPressed: null)
+          IconButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+              onPressed: null),
+          IconButton(
+              icon: Icon(
+                Icons.share,
+                color: Colors.white,
+              ),
+              onPressed: null)
         ],
       ),
       bottomNavigationBar: Container(
@@ -50,7 +60,10 @@ class _MyHomeState extends State<MyHome> {
                     //color: Colors.red,
                     border: Border.all(color: Colors.white, width: 4.0)),
                 child: IconButton(
-                  icon: Icon(Icons.star_rate,color: Colors.white,),
+                  icon: Icon(
+                    Icons.star_rate,
+                    color: Colors.white,
+                  ),
                   onPressed: () {},
                 ),
               ),
@@ -65,9 +78,11 @@ class _MyHomeState extends State<MyHome> {
                     //color: Colors.red,
                     border: Border.all(color: Colors.white, width: 4.0)),
                 child: IconButton(
-                  icon: Icon(Icons.power_settings_new,color: Colors.white,),
-                  onPressed: () {
-                  },
+                  icon: Icon(
+                    Icons.power_settings_new,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
                 ),
               )
             ],
@@ -80,34 +95,38 @@ class _MyHomeState extends State<MyHome> {
           children: [
             // study now grid
             InkWell(
-              onTap: (){
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context){
-                      return Lessons();
-                    }));
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return Lessons();
+                }));
               },
               child: ListTile(
                 title: Container(
                   height: 100,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom:12.0),
-                    child: CircleAvatar(
-                        child: Image.asset("images/study.png")),
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: CircleAvatar(child: Image.asset("images/study.png")),
                   ),
                 ),
                 subtitle: Padding(
-                  padding: const EdgeInsets.only(bottom:22.0,left: 9.0),
-                  child: Text("Tangira wige",style: TextStyle(
-                      fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                  ),),
+                  padding: const EdgeInsets.only(bottom: 22.0, left: 9.0),
+                  child: Text(
+                    "Tangira wige",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ),
-  //traffic light grid          // traffic light grid
+            //traffic light grid          // traffic light grid
 
             InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
                   return Trafficlight_screen();
                 }));
               },
@@ -115,22 +134,27 @@ class _MyHomeState extends State<MyHome> {
                 title: Container(
                   height: 100,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom:12.0),
+                    padding: const EdgeInsets.only(bottom: 12.0),
                     child: CircleAvatar(
                         child: Image.asset("images/traffic_light.jpg")),
                   ),
                 ),
                 subtitle: Container(
-                  child: Text("Amatara Yo mu muhanda",style: TextStyle(
-                      fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                  ),),
+                  child: Text(
+                    "Amatara Yo mu muhanda",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ),
-       // // car sign grid     // car sign grid           // car sign grid
+            // // car sign grid     // car sign grid           // car sign grid
             InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
                   return CarSigns();
                 }));
               },
@@ -138,23 +162,28 @@ class _MyHomeState extends State<MyHome> {
                 title: Container(
                   height: 100,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom:12.0),
-                    child: CircleAvatar(
-                        child: Image.asset("images/carSign.png")),
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child:
+                        CircleAvatar(child: Image.asset("images/carSign.png")),
                   ),
                 ),
                 subtitle: Padding(
-                  padding: const EdgeInsets.only(bottom:22.0,left: 9.0),
-                  child: Text("Ibimenyetso by'imodoka",style: TextStyle(
-                      fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                  ),),
+                  padding: const EdgeInsets.only(bottom: 22.0, left: 9.0),
+                  child: Text(
+                    "Ibimenyetso by'imodoka",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ),
 // question and answer grid
             InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
                   return Questions_and_answers();
                 }));
               },
@@ -162,24 +191,29 @@ class _MyHomeState extends State<MyHome> {
                 title: Container(
                   height: 100,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom:12.0),
+                    padding: const EdgeInsets.only(bottom: 12.0),
                     child: CircleAvatar(
                         child: Image.asset("images/questions.jpg")),
                   ),
                 ),
                 subtitle: Padding(
-                  padding: const EdgeInsets.only(bottom:22.0,left: 9.0),
-                  child: Text("Soma Ibibazo",style: TextStyle(
-                      fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                  ),),
+                  padding: const EdgeInsets.only(bottom: 22.0, left: 9.0),
+                  child: Text(
+                    "Soma Ibibazo",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ),
 
 // Quiz grid
             InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
                   return Quiz_screen();
                 }));
               },
@@ -187,43 +221,48 @@ class _MyHomeState extends State<MyHome> {
                 title: Container(
                   height: 100,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom:12.0),
+                    padding: const EdgeInsets.only(bottom: 12.0),
                     child: CircleAvatar(
                         child: Image.asset("images/quiz_logo.png")),
                   ),
                 ),
                 subtitle: Padding(
-                  padding: const EdgeInsets.only(bottom:22.0,left: 9.0),
-                  child: Text("Imenyereze",style: TextStyle(
-                      fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                  ),),
+                  padding: const EdgeInsets.only(bottom: 22.0, left: 9.0),
+                  child: Text(
+                    "Imenyereze",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ),
-
 
             ListTile(
               title: Container(
                 height: 100,
                 child: Padding(
-                  padding: const EdgeInsets.only(bottom:12.0),
-                  child: CircleAvatar(
-                      child: Image.asset("images/share.png")),
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: CircleAvatar(child: Image.asset("images/share.png")),
                 ),
               ),
               subtitle: Padding(
-                padding: const EdgeInsets.only(bottom:22.0,left: 9.0),
-                child: Text("Sangiza Abandi",style: TextStyle(
-                    fontSize: 16.0,color: Colors.pink,fontWeight: FontWeight.w700
-                ),),
+                padding: const EdgeInsets.only(bottom: 22.0, left: 9.0),
+                child: Text(
+                  "Sangiza Abandi",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.pink,
+                      fontWeight: FontWeight.w700),
+                ),
               ),
             ),
-
           ],
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         ),
       ),
     );
   }
 }
-
