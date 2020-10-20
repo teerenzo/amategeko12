2cfc9305ea9e260538b725e1d8b90a07d1d6f202
teerenzo <renzahoemmanuel8@gmail.com>
2020-10-20 04:24:55 +0300

    tee

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -112,6 +112,7 @@ class _QuizScreenState extends State<QuizScreen> {
     if (apl == right) {
       _totalScore += 1;
       showDialog(
+        barrierDismissible: false,
         context: context,
         builder: (context) {
           return AlertDialog(
@@ -147,6 +148,7 @@ class _QuizScreenState extends State<QuizScreen> {
       );
     } else if (apl == 2 && right == 4) {
       showDialog(
+        barrierDismissible: false,
         context: context,
         builder: (context) {
           return AlertDialog(
@@ -182,6 +184,7 @@ class _QuizScreenState extends State<QuizScreen> {
       );
     } else {
       showDialog(
+        barrierDismissible: false,
         context: context,
         builder: (context) {
           return AlertDialog(
